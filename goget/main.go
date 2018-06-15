d76fb2bd73bcc1e02fbc7f2a15634b71c3a39867
StevenZack <stevenzack@qq.com>
2018-06-15 21:15:06 +0800

    Just a Backup

---
+++ goget/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	url := os.Args[1]
+	url = handleUrl(url)
+	cmd := exec.Command("go", "get", url)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	e := cmd.Run()
+	if e != nil {
+		fmt.Println(e)
+		return
+	}
+}
+func handleUrl(url string) string {
+	s := url
+	preffix, suffix := "https://", ".git"
+	if len(s) > len(preffix) && s[:len(preffix)] == preffix {
+		s = s[len(preffix):]
+	}
+	if len(s) > len(suffix) && s[len(s)-len(suffix):] == suffix {
+		s = s[:len(s)-len(suffix)]
+	}
+	return s
+}
